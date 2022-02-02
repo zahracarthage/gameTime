94d1f715603e7bed7087f58f7c96ba3d402e55d4
Zahra carthage <zahracarthage.chouchene@esprit.tn>
2022-02-02 13:00:28 +0100

    SignUp Page

--- app/src/main/java/com/example/gametime/view/SignInFragment.kt
+++ app/src/main/java/com/example/gametime/view/SignInFragment.kt
@@ -5,17 +5,29 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
 import com.example.gametime.R
+import com.example.gametime.databinding.FragmentSignInBinding
 
 
 class SignInFragment : Fragment() {
+
+    private lateinit var binding: FragmentSignInBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sign_in, container, false)
+        binding = FragmentSignInBinding.inflate(inflater, container, false)
+       // return inflater.inflate(R.layout.fragment_sign_in, container, false)
+        binding.signUpbtn.setOnClickListener(
+
+                Navigation.createNavigateOnClickListener(R.id.action_signInFragment_to_signUpFragment)
+            )
+
+
+        return binding.root
     }
 
 
