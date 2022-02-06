b5a129297324ac6930e81878f7586d5d93b554b7
Zahra carthage <zahracarthage.chouchene@esprit.tn>
2022-02-06 18:55:09 +0100

    bottom navigation + bottom appbar

--- app/src/main/java/com/example/gametime/view/mainFragment.kt
+++ app/src/main/java/com/example/gametime/view/mainFragment.kt
@@ -1,5 +1,6 @@
 package com.example.gametime.view
 
+import android.app.ActionBar
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -7,20 +8,21 @@ import android.view.View
 import android.view.ViewGroup
 import com.example.gametime.R
 import com.example.gametime.databinding.FragmentMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationItemView
 
 
 class mainFragment : Fragment() {
 
-lateinit var binding: FragmentMainBinding
+    lateinit var binding: FragmentMainBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         binding = FragmentMainBinding.inflate(inflater,container,false)
+        binding.navbar.MyActionBar.background=null
 
         return binding.root
     }
-
 
 }
